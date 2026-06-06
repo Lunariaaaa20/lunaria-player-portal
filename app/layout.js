cd39a21c7b400fd4c73d4637daa04fb155baf1f5
Lunariaaaa20 <lunariarp43@gmail.com>
2026-06-06 09:47:42 +0000

    Add Lunaria fantasy ambience to layout

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
     export default function RootLayout({ children }) {
       return (
           <html lang="id">
-                <body>{children}</body>
+                <body><div className="lunaria-ambience" />{children}</body>
                     </html>
                       );
                       }
